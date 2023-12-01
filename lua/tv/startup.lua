931f44ce3142c1822a14bb72375f98bbdbd220b6
jonko0493 <jonko0493@protonmail.com>
2023-11-30 19:57:41 -0800

    Concurrent, single script model

--- lua/tv/startup.lua
+++ lua/tv/startup.lua
@@ -14,20 +14,12 @@ local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 local decoder = dfpwm.make_decoder()
 
-local function doTV()
-    gpu.fill()
-    local imgBuf = http.get("https://SERVER_URI/channel/01/"..px_w, {}, true)
-    local b = imgBuf.read(1)
-    local imgBin = {}
-    while b do
-        imgBin[#imgBin+1] = ("<I1"):unpack(b)
-        b = imgBuf.read(1)
-    end
-    image = gpu.decodeImage(table.unpack(imgBin))
-    gpu.drawBuffer(1, 1, px_w, 1, image.getAsBuffer())
-    gpu.sync()
+local imgRequest = { url = "https://SERVER_URI/channel/01/"..px_w, binary = true }
+local audioRequest = { url = "https://SERVER_URI/channel/01/audio" }
+local imgBuf = http.get(imgRequest)
+local rawAudio = http.get(audioRequest)
 
-    local rawAudio = http.get("https://SERVER_URI/channel/01/audio")
+local function playAudio()
     local line = rawAudio.readLine()
     while line ~= nil do
         local buffer = decoder(line)
@@ -38,6 +30,35 @@ local function doTV()
 
         line = rawAudio.readLine()
     end
+end
+
+local function waitForImgDownload()    
+    local event, url, handle
+    if url ~= imgRequest.url then
+        event, url, handle = os.pullEvent("http_success")
+        if url == imgRequest.url then
+            imgBuf = handle
+        end
+    end
+end
+
+
+local function doTV()
+    gpu.fill()
+    local b = imgBuf.read(1)
+    local imgBin = {}
+    while b do
+        imgBin[#imgBin+1] = ("<I1"):unpack(b)
+        b = imgBuf.read(1)
+    end
+    image = gpu.decodeImage(table.unpack(imgBin))
+    gpu.drawBuffer(1, 1, px_w, 1, image.getAsBuffer())
+    gpu.sync()
+    http.request(imgRequest)
+    
+    parallel.waitForAll(playAudio, waitForImgDownload)
+
+    rawAudio = http.get(audioRequest)
 end
 
 local function controlTV()
